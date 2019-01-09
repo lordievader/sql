836730aa88dd8303e165d4fd3cdd7329404a1050
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-01-09 09:38:54 +0100

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sql',
-    version='1.0',
+    version='1.0.1',
     description='SQL abstraction library',
     author='Olivier van der Toorn',
     author_email='oliviervdtoorn@gmail.com',
