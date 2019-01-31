1ae5c66332606661431c10ceccbf2317ada8b760
Olivier van der Toorn <oliviervdtoorn@gmail.com>
2019-01-31 13:41:38 +0100

    Raise loglevel impala_client
    Hides the logging output of impyla.

--- impala_client.py
+++ impala_client.py
@@ -4,12 +4,14 @@ Description:    Example implementation of an ImpalaClient.
 """
 import sys
 import pickle
+import logging
 from sql.connectors.impala import ImpalaClient
 
 
 def main():
     """Main function, reads flags, builds a client and queries the cluster.
     """
+    logging.basicConfig(level='ERROR')
     impala_client = ImpalaClient()
     line = sys.stdin.read()
     if line:
